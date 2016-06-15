6973cc67521e53f31479d3247e3ec4124a67fd39
graywolf336 <graywolf336@craftyn.com>
2016-06-15 10:27:53 -0500

    Add the static api class, not sure if this should be added or not.

---
+++ src/main/java/com/graywolf336/trailingperspective/TrailingPerspectiveAPI.java
@@ -0,0 +1,27 @@
+package com.graywolf336.trailingperspective;
+
+import com.graywolf336.trailingperspective.interfaces.ITrailerManager;
+
+/**
+ * Contains static api for interacting with the plugin.
+ *
+ * @author graywolf336
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+public class TrailingPerspectiveAPI {
+    private static TrailingPerspectiveMain plugin;
+    
+    protected static void setPlugin(TrailingPerspectiveMain pl) {
+        plugin = pl;
+    }
+    
+    /**
+     * Gets the a valid {@link ITrailerManager} instance.
+     * 
+     * @return {@link ITrailerManager} instance
+     */
+    public static ITrailerManager getTrailerManager() {
+        return plugin.getTrailerManager();
+    }
+}
